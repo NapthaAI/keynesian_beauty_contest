bbb8c88e187ef466c9f77a47524a6940a71fbd5e
Richard Blythman <richardblythman@gmail.com>
2024-09-26 17:53:39 +0100

    update task engine

--- keynesian_beauty_contest/run.py
+++ keynesian_beauty_contest/run.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 import asyncio
 import math
-from naptha_sdk.client.node import Node
-from naptha_sdk.schemas import AgentRunInput
 from naptha_sdk.task import Task as Agent
 from naptha_sdk.utils import get_logger, load_yaml
-from keynesian_beauty_contest.schemas import InputSchema
 import time
 
 logger = get_logger(__name__)
 
-async def run_agent(agent, name):
-    response = await agent(agent_name=name)
-    return response
-
-async def run(inputs, worker_nodes=None, orchestrator_node=None, flow_run=None, cfg=None):
+async def run(inputs, worker_nodes=None, orchestrator_node=None, cfg=None, task_engine=None):
     logger.info(f"Inputs: {inputs}")
 
     num_nodes = len(worker_nodes)
@@ -28,8 +21,8 @@ async def run(inputs, worker_nodes=None, orchestrator_node=None, flow_run=None, 
     for i in range(num_agents):
         node_index = min(i // agents_per_node, num_nodes - 1)
         name = f"Agent_{i}"
-        agent = Agent(name=name, fn="random_number_agent", worker_node=worker_nodes[node_index], orchestrator_node=orchestrator_node, flow_run=flow_run)
-        tasks.append(run_agent(agent, name))
+        agent = Agent(name=name, fn="random_number_agent", worker_node=worker_nodes[node_index], orchestrator_node=orchestrator_node, task_engine=task_engine)
+        tasks.append(agent(agent_name=name))
 
     results = await asyncio.gather(*tasks)
 
@@ -42,19 +35,23 @@ async def run(inputs, worker_nodes=None, orchestrator_node=None, flow_run=None, 
 
 
 if __name__ == "__main__":
+    from keynesian_beauty_contest.schemas import InputSchema
+    from naptha_sdk.schemas import AgentRunInput
+    from naptha_sdk.client.node import Node
+
     cfg_path = "keynesian_beauty_contest/component.yaml"
     cfg = load_yaml(cfg_path)
 
     inputs = {
         "num_agents": 2,
     }
-    flow_run = {"consumer_id": "user:18837f9faec9a02744d308f935f1b05e8ff2fc355172e875c24366491625d932f36b34a4fa80bac58db635d5eddc87659c2b3fa700a1775eb4c43da6b0ec270d", 
+    agent_run = {"consumer_id": "user:18837f9faec9a02744d308f935f1b05e8ff2fc355172e875c24366491625d932f36b34a4fa80bac58db635d5eddc87659c2b3fa700a1775eb4c43da6b0ec270d", 
                 "agent_name": "random_agent", "agent_source_url": "https://github.com/NapthaAI/keynesian_beauty_contest", "agent_version": "0.1", "worker_nodes": ["http://localhost:7001"]} 
-    flow_run = AgentRunInput(**flow_run)
+    agent_run = AgentRunInput(**agent_run)
     inputs = InputSchema(**inputs)
     orchestrator_node = Node("http://localhost:7001")
-    worker_nodes = [Node(worker_node) for worker_node in flow_run.worker_nodes]
+    worker_nodes = [Node(worker_node) for worker_node in agent_run.worker_nodes]
 
-    response = asyncio.run(run(inputs, worker_nodes, orchestrator_node, flow_run, cfg))
+    response = asyncio.run(run(inputs, worker_nodes, orchestrator_node, cfg))
     print(response)
-    print("Flow Run: ", flow_run)
+    print("Agent Run: ", agent_run)
